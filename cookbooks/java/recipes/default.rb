2aeffcbc604ea1b4d5968e676e905d7c3903d1eb
Colin Jones <trptcolin@gmail.com>
2013-07-04 17:43:48 -0500

    Trim a java version until we need it

--- cookbooks/java/recipes/default.rb
+++ cookbooks/java/recipes/default.rb
@@ -1,5 +1,4 @@
-package_names = ["openjdk-7-jre-headless", "openjdk-7-jdk",
-                 "openjdk-6-jre-headless", "openjdk-6-jdk"]
+package_names = ["openjdk-7-jre-headless", "openjdk-7-jdk"]
 package_names.each do |name|
   package name
 end
