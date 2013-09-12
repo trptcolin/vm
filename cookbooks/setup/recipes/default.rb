c35e6241b4d93b1b5a5e55004a43b71e8c333bad
Colin Jones <trptcolin@gmail.com>
2013-09-12 10:56:05 -0500

    Do apt-get update once daily

--- cookbooks/setup/recipes/default.rb
+++ cookbooks/setup/recipes/default.rb
@@ -1,3 +1,14 @@
+seconds_in_a_day = 86400
+
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  not_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+      File.mtime('/var/lib/apt/periodic/update-success-stamp') > Time.now - seconds_in_a_day
+  end
+end
+
 package "build-essential"
 package "ruby1.9.1-dev" # for chef
 package "zip" # so we can look inside jarfiles
